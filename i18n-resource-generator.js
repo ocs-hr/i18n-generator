77a630b6dd029daded0011cb3f9a8d46881edcbe
Erlend Torsvik <torsvik@hotmail.com>
2015-10-02 14:23:20 +0200

    Added header generation of files.

--- i18n-resource-generator.js
+++ i18n-resource-generator.js
@@ -23,222 +23,230 @@
  */
 
 var fs = require('fs'),
-	beautify = require('js-beautify').js_beautify, module = module || {};
+    beautify = require('js-beautify').js_beautify, module = module || {};
 
 var variableName = {
-	i18nGo: 'i18n=>',
-	nestStart: '=>',
-	nestEnd: '<='
+    i18nGo: 'i18n=>',
+    nestStart: '=>',
+    nestEnd: '<='
 };
 
 var variable = {
-	split: '|',
-	language: [],
-	nestObject: [],
-	i18n: {}
+    split: '|',
+    language: [],
+    nestObject: [],
+    i18n: {}
 };
 
 function resetVariable() {
-	variable = {
-		split: '|',
-		language: [],
-		nestObject: [],
-		i18n: {}
-	};
+    variable = {
+        split: '|',
+        language: [],
+        nestObject: [],
+        i18n: {}
+    };
 }
 
 // http://stackoverflow.com/questions/5484673/javascript-how-to-dynamically-create-nested-objects-using-object-names-given-by
 function assign(obj, keyPath, value) {
-	var lastKeyIndex = keyPath.length - 1;
-	for (var i = 0; i < lastKeyIndex; ++i) {
-		var key = keyPath[i];
-		if (!(key in obj)) {
-			obj[key] = {};
-		}
-		obj = obj[key];
-	}
-	obj[keyPath[lastKeyIndex]] = value;
+    var lastKeyIndex = keyPath.length - 1;
+    for (var i = 0; i < lastKeyIndex; ++i) {
+        var key = keyPath[i];
+        if (!(key in obj)) {
+            obj[key] = {};
+        }
+        obj = obj[key];
+    }
+    obj[keyPath[lastKeyIndex]] = value;
 }
 
 function magicSplit(data) {
-	var output = [],
-		index = 0,
-		tempString = '',
-		isInside = false;
-	
-	while (data.length) {
-		if (data[0] === '"') {
-			isInside = !isInside;
-			data = data.slice(1, data.length);
-			continue;
-		} else if (data[0] === '\\' && isInside) {
-			tempString += data[1];
-			data = data.slice(2, data.length);
-			continue;
-		} else if (data[0] === variable.split && !isInside) {
-			output[index] = tempString;
-			tempString = '';
-			index++;
-			data = data.slice(1, data.length);
-			continue;
-		}
-		
-		tempString += data[0];
-		data = data.slice(1, data.length);
-	}
-	
-	output[index] = tempString;
-	// console.log(output)
-	return output;
+    var output = [],
+        index = 0,
+        tempString = '',
+        isInside = false;
+    
+    while (data.length) {
+        if (data[0] === '"') {
+            isInside = !isInside;
+            data = data.slice(1, data.length);
+            continue;
+        } else if (data[0] === '\\' && isInside) {
+            tempString += data[1];
+            data = data.slice(2, data.length);
+            continue;
+        } else if (data[0] === variable.split && !isInside) {
+            output[index] = tempString;
+            tempString = '';
+            index++;
+            data = data.slice(1, data.length);
+            continue;
+        }
+        
+        tempString += data[0];
+        data = data.slice(1, data.length);
+    }
+    
+    output[index] = tempString;
+    // console.log(output)
+    return output;
 }
 
 function i18nGenerating(data) {
-	
-	var output = magicSplit(data);
-	// i18n=> (i18nGo)
-	if (output[0].indexOf(variableName.i18nGo) !== -1) {
-		for (var i = 1; i < output.length; i++) {
-			var lang = output[i].trim();
-			variable.language.push(lang);
-			variable.i18n[lang] = {};
-		}
-		
-		return;
-	}
-	
-	// => (nestStart)
-	if (output[0].indexOf(variableName.nestStart) !== -1) {
-		variable.nestObject.push(output[0].trim().replace(variableName.nestStart, '').trim());
-		return;
-	}
-	
-	// <= (nestEnd)
-	if (output[0].indexOf(variableName.nestEnd) !== -1) {
-		variable.nestObject = [];
-		return;
-	}
-	
-	// generating json object
-	for (var j = 1; j < output.length; j++) {
-		if (variable.nestObject.length) {
-			variable.nestObject.push(output[0].trim());
-			if (output[j].trim()) {
-				assign(variable.i18n[variable.language[j - 1]], variable.nestObject, output[j].trim());
-			}
-			variable.nestObject.pop();
-		} else if (variable.nestObject.length === 0 && output[j].trim()) {
-			variable.i18n[variable.language[j - 1]][output[0].trim()] = output[j].trim();
-		}
-	}
+    
+    var output = magicSplit(data);
+    // i18n=> (i18nGo)
+    if (output[0].indexOf(variableName.i18nGo) !== -1) {
+        for (var i = 1; i < output.length; i++) {
+            var lang = output[i].trim();
+            variable.language.push(lang);
+            variable.i18n[lang] = {};
+        }
+        
+        return;
+    }
+    
+    // => (nestStart)
+    if (output[0].indexOf(variableName.nestStart) !== -1) {
+        variable.nestObject.push(output[0].trim().replace(variableName.nestStart, '').trim());
+        return;
+    }
+    
+    // <= (nestEnd)
+    if (output[0].indexOf(variableName.nestEnd) !== -1) {
+        variable.nestObject = [];
+        return;
+    }
+    
+    // generating json object
+    for (var j = 1; j < output.length; j++) {
+        if (variable.nestObject.length) {
+            variable.nestObject.push(output[0].trim());
+            if (output[j].trim()) {
+                assign(variable.i18n[variable.language[j - 1]], variable.nestObject, output[j].trim());
+            }
+            variable.nestObject.pop();
+        } else if (variable.nestObject.length === 0 && output[j].trim()) {
+            variable.i18n[variable.language[j - 1]][output[0].trim()] = output[j].trim();
+        }
+    }
 
+}
+
+function i18nGenerateFileHeader(output, outfile) {
+    variable.i18n.forEach(function (lang) {
+        var header = "// Generated file using i18n-resource-generator. Please edit the file " + outfile + ".txt";
+        
+        var relativePathAndName = output + '/' + lang + '/' + outfile + '.json';
+        fs.writeFileSync(relativePathAndName, header, { flag: 'w' });
+    });
 }
 
 function i18nFileGenerate(output, options, outfile) {
-	
-	for (var lang in variable.i18n) {
-		var writeText = JSON.stringify(variable.i18n[lang]);
-		if (options) {
-			writeText = beautify(writeText, options);
-		}
-		var relativePathAndName = output + '/' + lang + '/' + outfile + '.json';
-		fs.writeFileSync(relativePathAndName, writeText, { flag: 'w' });
-	}
-
+    
+    for (var lang in variable.i18n) {
+        var writeText = JSON.stringify(variable.i18n[lang]);
+        if (options) {
+            writeText = beautify(writeText, options);
+        }
+        var relativePathAndName = output + '/' + lang + '/' + outfile + '.json';
+        fs.writeFileSync(relativePathAndName, writeText, { flag: 'w' });
+    }
 }
 
 function readFileAndGenerating(input, split) {
-	var data;
-	
-	try {
-		data = fs.readFileSync(input);
-	} catch (e) {
-		data = input;
-	}
-	
-	// reset variable
-	resetVariable();
-	
-	var remaining = '';
-	
-	// setting up the splitter
-	// default is pipe |
-	if (!split) {
-		split = 'pipe';
-	}
-	
-	switch (split) {
-		case 'csv':
-			variable.split = ',';
-			break;
-		case 'tsv':
-			variable.split = '\t';
-			break;
-		case 'pipe':
-			variable.split = '|';
-			break;
-		default:
-			variable.split = split;
-	}
-	
-	remaining += data;
-	var index = remaining.indexOf('\n');
-	var last = 0;
-	while (index > -1) {
-		var line = remaining.substring(last, index);
-		last = index + 1;
-		i18nGenerating(line);
-		index = remaining.indexOf('\n', last);
-	}
-	
-	remaining = remaining.substring(last);
-	
-	if (remaining.length > 0) {
-		i18nGenerating(remaining);
-	}
+    var data;
+    
+    try {
+        data = fs.readFileSync(input);
+    } catch (e) {
+        data = input;
+    }
+    
+    // reset variable
+    resetVariable();
+    
+    var remaining = '';
+    
+    // setting up the splitter
+    // default is pipe |
+    if (!split) {
+        split = 'pipe';
+    }
+    
+    switch (split) {
+        case 'csv':
+            variable.split = ',';
+            break;
+        case 'tsv':
+            variable.split = '\t';
+            break;
+        case 'pipe':
+            variable.split = '|';
+            break;
+        default:
+            variable.split = split;
+    }
+    
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    var last = 0;
+    while (index > -1) {
+        var line = remaining.substring(last, index);
+        last = index + 1;
+        i18nGenerating(line);
+        index = remaining.indexOf('\n', last);
+    }
+    
+    remaining = remaining.substring(last);
+    
+    if (remaining.length > 0) {
+        i18nGenerating(remaining);
+    }
 }
 
 module.exports = function (input, output, options, split) {
-	
-	
-	var isExist = fs.existsSync(output);
-	
-	if (!isExist) {
-		fs.mkdirSync(output);
-	}
-	
-	readFileAndGenerating(input, split);
-	
-	var struct = input.split("/");
-	var outFile = struct[struct.length - 1].replace(/\.[a-zA-Z]{3}/ig, "");
-	for (var lang in variable.i18n) {
-		var folder = output + "/" + lang;
-		if (!fs.existsSync(folder)) {
-			fs.mkdirSync(folder);
-		}
-	}
-	i18nFileGenerate(output, options || null, outFile);
+    
+    
+    var isExist = fs.existsSync(output);
+    
+    if (!isExist) {
+        fs.mkdirSync(output);
+    }
+    
+    readFileAndGenerating(input, split);
+    
+    var struct = input.split("/");
+    var outFile = struct[struct.length - 1].replace(/\.[a-zA-Z]{3}/ig, "");
+    for (var lang in variable.i18n) {
+        var folder = output + "/" + lang;
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder);
+        }
+    }
+    i18nGenerateFileHeader(output, outFile);
+    i18nFileGenerate(output, options || null, outFile);
 
 };
 
 module.exports.get = function (input, split, cb) {
-	
-	if (typeof split === 'function') {
-		cb = split;
-		split = 'pipe';
-	}
-	readFileAndGenerating(input, split);
     
-    if (typeof(cb) === "function")
-    {
-        cb.call(null, null, variable.i18n);  
+    if (typeof split === 'function') {
+        cb = split;
+        split = 'pipe';
+    }
+    readFileAndGenerating(input, split);
+    
+    if (typeof (cb) === "function") {
+        cb.call(null, null, variable.i18n);
     }
 	
 };
 
 /* browser window */
 if (typeof window !== 'undefined') {
-	// If we're running a web page
-	window.i18n = module.exports.get;
+    // If we're running a web page
+    window.i18n = module.exports.get;
 }
 
 // basic
